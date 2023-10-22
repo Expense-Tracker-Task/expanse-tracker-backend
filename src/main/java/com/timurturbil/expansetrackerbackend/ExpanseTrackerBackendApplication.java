21046ca50190f12ad233379a60b406956bd0ac58
timurturbil <timurturbill@gmail.com>
2023-10-22 12:33:48 +0300

    remove logger from main application file

--- src/main/java/com/timurturbil/expansetrackerbackend/ExpanseTrackerBackendApplication.java
+++ src/main/java/com/timurturbil/expansetrackerbackend/ExpanseTrackerBackendApplication.java
@@ -2,17 +2,12 @@ package com.timurturbil.expansetrackerbackend;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ExpanseTrackerBackendApplication {
-    private static final Logger log = LoggerFactory.getLogger(ExpanseTrackerBackendApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(ExpanseTrackerBackendApplication.class, args);
     }
